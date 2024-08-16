91e533a2721b925183fddd0a9208530e418047c9
xpat <patrycja.kruczynska@gmail.com>
2024-08-16 10:25:03 +0200

    added route modify and delete

--- app.js
+++ app.js
@@ -50,4 +50,16 @@ app.get("/api/books/:id", (req, res, next) => {
     .catch((error) => res.status(404).json({ error }));
 });
 
+app.put("/api/books/:id", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Book modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Book supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
 module.exports = app;
