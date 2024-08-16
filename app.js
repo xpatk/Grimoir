5f33d1ed8ad9183faae55ff6ac17869b1fde129a
xpat <patrycja.kruczynska@gmail.com>
2024-08-16 10:43:39 +0200

    routes books

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
-const Book = require("./models/Book");
+const booksRoutes = require("./routes/books");
+const bodyParser = require("body-parser");
 
 app.use(express.json());
 
@@ -27,39 +27,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/books", (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body,
-  });
-  book
-    .save()
-    .then(() => res.status(201))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use(bodyParser.json());
 
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/books/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-app.put("/api/books/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Book modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.delete("/api/books/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Book supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/books", booksRoutes);
 
 module.exports = app;
