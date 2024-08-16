50066c5eca0183cc6f4696d2fa0fa9ab9a8ba422
xpat <patrycja.kruczynska@gmail.com>
2024-08-16 11:12:01 +0200

    controllers

--- routes/books.js
+++ routes/books.js
@@ -1,41 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const Book = require("../models/Book");
+const booksCtrl = require("../controllers/books");
 
-router.post("/api/books", (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body,
-  });
-  book
-    .save()
-    .then(() => res.status(201))
-    .catch((error) => res.status(400).json({ error }));
-});
+router.post("/", booksCtrl.createBook);
 
-router.put("/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Book modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+router.put("/:id", booksCtrl.modifyBook);
 
-router.delete("/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Book supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+router.delete("/:id", booksCtrl.deleteBook);
 
-router.get("/", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
+router.get("/", booksCtrl.getAllBooks);
 
-router.get("/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
-});
+router.get("/:id", booksCtrl.getOneBook);
 
 module.exports = router;
