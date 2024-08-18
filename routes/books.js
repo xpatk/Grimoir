c818ffe0880823b497ac5d7456b170ef56dffb11
xpat <patrycja.kruczynska@gmail.com>
2024-08-19 00:04:46 +0200

    auth js

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const auth = require("auth");
 const router = express.Router();
 
 const booksCtrl = require("../controllers/books");
 
-router.post("/", booksCtrl.createBook);
+router.post("/", auth, booksCtrl.createBook);
 
-router.put("/:id", booksCtrl.modifyBook);
+router.put("/:id", auth, booksCtrl.modifyBook);
 
-router.delete("/:id", booksCtrl.deleteBook);
+router.delete("/:id", auth, booksCtrl.deleteBook);
 
 router.get("/", booksCtrl.getAllBooks);
 
