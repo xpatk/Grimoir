f3daca68c08916ebcd95b9e0bbadd289123e4907
xpat <patrycja.kruczynska@gmail.com>
2024-08-18 23:48:13 +0200

    added token - installed jsonwebtoken

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
@@ -35,7 +36,9 @@ exports.login = (req, res, next) => {
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: "TOKEN",
+                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+                  expiresIn: "24h",
+                }),
               });
             }
           })
